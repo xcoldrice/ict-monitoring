7689d164fe939079912db767f197321035c28eb5
dev <dev.pagasa@gmail.com>
2022-03-08 16:29:58 +0800

    prod env

---
+++ config/production/radars.php
@@ -0,0 +1,39 @@
+<?php
+
+    return [
+                'eec' => [
+                            'radars' => [
+                                'tagaytay',
+                                'subic',
+                                'mactan',
+                                'bohol',
+                                'iloilo',
+                                'hinatuan',
+                            ]
+                        ],
+                'jrc' => [
+                            'radars' => [
+                                'aparri',
+                                'virac',
+                                'guiuan',
+                            ]
+                        ],
+                'selex' => [
+                                'radars'=> [
+                                    'baguio',
+                                    'baler'
+
+                                ]
+                        ],
+                'iris' => [
+                            'radars' => [
+
+                            ]
+
+                        ],
+                'mosaic' => [
+                    'radars' => [
+                        'mosaic'
+                    ]
+                ]
+    ];
