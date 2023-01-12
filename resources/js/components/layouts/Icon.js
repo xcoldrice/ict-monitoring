2121687f4a2003867d632bf9141aa4723cbe5ac1
tek <dev.pagasa@gmail.com>
2023-01-12 14:49:37 +0800

    update label

--- resources/js/components/layouts/Icon.js
+++ resources/js/components/layouts/Icon.js
@@ -1,3 +1,4 @@
+import moment from "moment";
 import React, { useContext } from "react";
 import { Badge, OverlayTrigger, Popover } from "react-bootstrap";
 import Moment from "react-moment";
@@ -7,18 +8,28 @@ function Icon(props) {
     let { children, tooltip, time, class: className, category, style } = props,
         { UNIXNOW, OFFSETS } = useContext(AppContext),
         { interval, threshold } = OFFSETS[category] ?? OFFSETS.default,
+        offset = interval,
         dataTime = "No Data.",
         timeDiff = "",
-        textColor = "secondary";
+        textColor = "secondary",
+        fileInterval = `Interval: ${offset / 1000 / 60} minutes.`;
 
     if (typeof time === "number") {
         dataTime = <Moment date={time} format="[Date:] MMMM DD, YYYY h:mm A" />;
-        timeDiff = <Moment fromNow date={time} />;
         textColor = "light";
+        let diffMin = moment().diff(time + offset, "minutes");
+        if (diffMin > 0 && diffMin < 60) {
+            timeDiff = `${diffMin} minutes late.`;
+        }
+        if (diffMin > 60 && diffMin < 1440) {
+            timeDiff = `${Math.floor(diffMin / 60)} hours late.`;
+        }
+        if (diffMin > 1440) {
+            timeDiff = `${Math.floor(diffMin / 1440)} days late.`;
+        }
     }
     const badge_color = () => {
-        let diff = new Date(UNIXNOW).getTime() - new Date(time).getTime(),
-            offset = interval + threshold;
+        let diff = new Date(UNIXNOW).getTime() - new Date(time).getTime();
 
         if (typeof time == "object") return "light";
 
@@ -35,6 +46,7 @@ function Icon(props) {
                 <Popover.Body>
                     {tooltip} <br />
                     {dataTime} <br />
+                    {fileInterval} <br />
                     {timeDiff}
                 </Popover.Body>
             </Popover>
