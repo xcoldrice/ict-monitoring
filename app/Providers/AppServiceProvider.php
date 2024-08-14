f0978723c590dd1526ebc9bc4ecd46f361fe7da1
tek <dev.pagasa@gmail.com>
2024-08-14 17:35:06 +0800

    ss

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace App\Providers;
 
-use Doctrine\DBAL\Schema\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -24,7 +23,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Schema::defaulStringLength(191);
         //
     }
 }
